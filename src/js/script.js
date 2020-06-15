bf61151deadd0d6f3137b1a70c08eda3dfc2f38c
baspovich <stfd20@yandex.ru>
2020-06-15 15:54:28 +0800

    add portfile screen

--- src/js/script.js
+++ src/js/script.js
@@ -7,3 +7,13 @@ $(document).ready(function(){
         nextArrow: '<button type="button" class="slick-next"><img src="../icons/down.svg" class="prev"></button>'
     });
   });
+
+$(document).ready(function(){
+    $('.slider_anybtn').slick({
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        prevArrow: '<button type="button" class="slick-prev slick-prev_color"><img src="../icons/down.svg" class="prev"></button>',
+        nextArrow: '<button type="button" class="slick-next slick-next_color"><img src="../icons/down.svg" class="prev"></button>'
+    });
+  });
