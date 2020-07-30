db29c86e87d9f435978466391799a101dcf6eaf2
baspovich <stfd20@yandex.ru>
2020-07-30 10:44:43 +0800

    v2 (without eng and rus version)

--- js/script.js
+++ js/script.js
@@ -1,46 +1,60 @@
 $(document).ready(function(){
-    $('.slider').slick({
-        infinite: true,
-        speed: 300,
-        slidesToShow: 1,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/down.svg" class="prev"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/down.svg" class="prev"></button>'
+    $('.portfile__slider').slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/prevArrow.svg" alt="arrow"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/nextArrow.svg" alt="arrow"></button>',
+        responsive: [
+            {
+                breakpoint: 769,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
     });
-  });
 
-$(document).ready(function(){
-    $('.slider_anybtn').slick({
-        infinite: true,
-        speed: 300,
-        slidesToShow: 1,
-        prevArrow: '<button type="button" class="slick-prev slick-prev_color"><img src="../icons/down.svg" class="prev"></button>',
-        nextArrow: '<button type="button" class="slick-next slick-next_color"><img src="../icons/down.svg" class="prev"></button>'
+    $('[data-modal=btn]').on('click', function() {
+        $('.overlay, #modal').fadeIn('slow');
     });
-  });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #thanks').fadeOut('slow');
+    });
 
-$('[data-modal=first-layout]').on('click', function() {
-  $('.overlay, #first-modal').fadeIn('slow');
-  });
-$('.modal__close').on('click', function() {
-  $('.overlay, #first-modal').fadeOut('slow');
+    $('.order__btn').each(function(i) {
+        $(this).on('click', function() {
+            $('#modal .modal__name').text($('.order__tittle').eq(i).text());
+            $('.overlay, #modal').fadeIn('slow');
+        })
+    });
+
+    $('.modal__form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+          type: "POST",
+          url: "mailer/smart.php",
+          data: $(this).serialize()
+        }).done(function() {
+          $(this).find("input").val("");
+          $('#modal').fadeOut();
+          $('.overlay, #thanks').fadeIn();
+          $("form").trigger('reset'); 
+        });
+        return false;
+    });
 });
 
-$('[data-modal=second-layout]').on('click', function() {
-  $('.overlay, #second-modal').fadeIn('slow');
-  });
-$('.modal__close').on('click', function() {
-  $('.overlay, #second-modal').fadeOut('slow');
+$('.sub-nav__link').on( 'click', function(){ 
+    const el = $(this);
+    const dest = el.attr('href'); // получаем направление
+    if(dest !== undefined && dest !== '') { // проверяем существование
+        $('html').animate({ 
+            scrollTop: $(dest).offset().top // прокручиваем страницу к требуемому элементу
+        }, 500 // скорость прокрутки
+        );
+    }
+    return false;
 });
 
 
-$('.page-arrow').on( 'click', function(){ 
-  const el = $(this);
-  const dest = el.attr('href'); // получаем направление
-  if(dest !== undefined && dest !== '') { // проверяем существование
-      $('html').animate({ 
-          scrollTop: $(dest).offset().top // прокручиваем страницу к требуемому элементу
-      }, 500 // скорость прокрутки
-      );
-  }
-  return false;
-});
+
+
